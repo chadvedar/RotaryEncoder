2008366be95ee3996dcc58ea997737c8a773a504
chadvedar <119101886+chadvedar@users.noreply.github.com>
2023-05-19 09:05:38 +0900

    c++ 99 compatible

--- RotaryEncoder.hpp
+++ RotaryEncoder.hpp
@@ -19,7 +19,7 @@
 #define _ROTARYENCODER
 
 #include "mbed.h"
-#include <chrono>
+//#include <chrono>
 
 
 class RotaryEncoder{
@@ -30,15 +30,15 @@ class RotaryEncoder{
         bool init();
         void start();
         float rpm;
-        float encoder_resolution = 0.005;
+        float encoder_resolution;
 
     private:
         InterruptIn CLK;
         InterruptIn SIG;
 
-        bool clk = 0;
-        int8_t dir = 1;
-        int encoder_pulse = 0;
+        bool clk;
+        int8_t dir;
+        int encoder_pulse;
 
         void set_clk_high();
         void set_clk_low();
